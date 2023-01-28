005f21bc98e21802f52fc2d6d44ce666bd0929c4
Charlesccharris <70536520+Charlesccharris@users.noreply.github.com>
2023-01-28 15:50:10 -0600

    Update Yahtzee.py
    Trying to see if I can change a list

--- Software Engineering/Yahtzee.py
+++ Software Engineering/Yahtzee.py
@@ -2,6 +2,7 @@ import random
 diceList = []
 dice = int()
 answer = ""
+score = ["Score Card:", "(1)Aces", "(2)Twos", "(3)Threes", "(4)Fours", "(5)Fives", "(6)Sixes", "(7)3 of a kind", "(8)4 of a kind", "(9)Full House", "(10)Sm Straight (Sequence of 4)", "(11)Lg. Straight (Sequence of 5)", "(12)YAHTZEE (5 of a kind)", "(13)Chance"]
 
 def Rules():
     print("Rules of Yahtzee")
@@ -19,10 +20,10 @@ def RollOne(diceList):
 def RollAgain(diceList):
     reroll = int(input("How many dice do you want to re-roll?:"))
     if(reroll > 5):
-        print("You only have 5 dice, so we'll re-roll them all")
+        print("You only have 5 dice, so we'll roll them all")
         reroll = 5
     elif(reroll < 1):
-        print("I guess you didn't want to re-roll any dice after all")
+        print("I guess you didn't want to roll any dice after all")
         reroll = 0
     
     if(reroll == 5):
@@ -37,11 +38,12 @@ def RollAgain(diceList):
             while(num < 1 or num > 5):
                 num = int(input("Please put in 1-5: "))
             diceList[num - 1] = RollDice()
-            
     print(diceList)
     
-def ScoreCard(diceList):
-    print("Score")
+def ScoreCard(diceList, score):
+    for x in score:
+        print(x)
+    score.append("Banana")
     
 def PlayYahtzee():
     num = int(0)
@@ -50,14 +52,15 @@ def PlayYahtzee():
         print("Round:", end = "")
         print(num)
         RollOne(diceList)
-        answer = input("Do you want to re-roll any dice? Y or N: ")
+        answer = input("Do you want to Re-roll any dice? Y or N: ")
         if('y' in answer or 'Y' in answer):
             RollAgain(diceList)
             answer = input("Do you want to Re-roll any dice? Y or N: ")
             if('y' in answer or 'Y' in answer):
                 RollAgain(diceList)
-        ScoreCard(diceList)
+        ScoreCard(diceList, score)
         diceList.clear() 
+    ScoreGame(ScoreCard)
 
 print("Welcome to Yahtzee")
 name = input("(R)ules or (P)lay? :")
