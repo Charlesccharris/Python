5841113d08b9915bcea6671d397117252d6dd4be
Charlesccharris <70536520+Charlesccharris@users.noreply.github.com>
2023-01-28 13:12:33 -0600

    Update Yahtzee.py

--- Software Engineering/Yahtzee.py
+++ Software Engineering/Yahtzee.py
@@ -1,25 +1,49 @@
 import random
-
-def PlayYahtzee():
-    print("BYE")
+diceList = []
+dice = int()
+answer = ""
 
 def Rules():
-    print("HI")
+    print("Rules of Yahtzee")
+    print("Yahtzee has 13 rounds")
+    print("Each round you roll 5 dice")
 
 def RollDice():
-    for x in range(5):
-        dice = random.randrange(1, 6)
-        print(dice)
+    return random.randrange(1,6)
 
-dice = int()
+def RollOne(diceList):
+    for x in range(5):
+        diceList.append(RollDice())
+    print (diceList)   
+
+def RollTwo(diceList):
+    print("Second")
+    
+def RollThree(diceList):
+    print("Third")
+    
+def ScoreCard(diceList):
+    print("Score")
+    
+def PlayYahtzee():
+    for round in range (13):
+        RollOne(diceList)
+        answer = input("Do you want to Re-roll any dice? Y or N: ")
+        if('y' in answer or 'Y' in answer):
+            RollTwo(diceList)
+            RollThree(diceList)
+        elif('n' in answer or 'N' in answer):
+            ScoreCard(diceList)
+        else:
+            
+        diceList.clear()
+        
 print("Welcome to Yahtzee")
 name = input("(R)ules or (P)lay? :")
 
 if('r' in name or 'R' in name):
-    print("Rules of Yahtzee")
     Rules()
 elif('p' in name or 'P' in name):
-    print("This is Yahtzee")
     PlayYahtzee()
 else:
     print("That is not a valid option")
