02feda82b80c7c96e835da494f939bc66185b784
Charlesccharris <70536520+Charlesccharris@users.noreply.github.com>
2023-01-28 12:30:45 -0600

    Update Yahtzee.py

--- Software Engineering/Yahtzee.py
+++ Software Engineering/Yahtzee.py
@@ -1,1 +1,29 @@
-print("Hello")
+import random
+
+
+print("Welcome to Yahtzee")
+name = input("(R)ules or (P)lay? :")
+
+if('r' in name or 'R' in name):
+    print("Rules of Yahtzee")
+    Rules.rule
+elif('p' in name or 'P' in name):
+    print("This is Yahtzee")
+    PlayYahtzee()
+else:
+    print("That is not a valid option")
+
+dice = int()
+
+#for x in range(5):
+    #dice = random.randrange(1, 6)
+   # print(dice)
+   
+class PlayYahtzee:
+    def play():
+        print("BYE")
+
+class Rules:
+    def rule():
+        print("HI")
+    
